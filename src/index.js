c9f42ea26bcbed0b7e44e265f51483bde5c91c8d
josserden <d-h11@ukr.net>
2021-11-24 19:55:34 +0200

    upd

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Global } from '@emotion/react';
-import { Provider } from 'react-redux';
-import store from './redux/store';
-import App from './components/App-redux';
+
+import App from './components/App';
 import { GlobalStyles } from './index.styled';
 
 ReactDOM.render(
   <React.StrictMode>
     <Global styles={GlobalStyles} />
-    <Provider store={store}>
-      <App />
-    </Provider>
+
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
